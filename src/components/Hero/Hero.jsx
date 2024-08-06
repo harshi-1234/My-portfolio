ff2ee15adf8f92a26d21035fbf7e3457f0e5ee67
Harshitha <abc123@gmail.com>
2024-08-06 21:36:48 +0530

    over14

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
@@ -8,8 +9,8 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Harshitha</h1>
         <p className={styles.description}>
-          I'm a passionate and curious computer science student with strong problem-solving skills. I have experience in full-stack web development, cloud computing, and Android studio domains. I'm looking for opportunities to
-          contribute and work in a challenging environment!
+        I'm a passionate and curious computer science student with strong problem-solving skills. I have experience in full-stack web development, cloud computing, and Android studio domains. I'm looking for opportunities to 
+        contribute and work in a challenging environment !
         </p>
         <a href="mailto:himakss31@gmail.com" className={styles.contactBtn}>
           Connect with me
